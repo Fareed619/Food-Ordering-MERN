d77f5272bad026e3b27acf4e90f7c78227689189
Fareeddeeb619 <fareeddeeb722@gmail.com>
2025-07-06 17:12:19 +0300

    Adding redirect for netlify and some changes

--- frontend/src/auth/Auth0ProviderWithNavigate.tsx
+++ frontend/src/auth/Auth0ProviderWithNavigate.tsx
@@ -10,10 +10,7 @@ const Auth0ProviderWithNavigate = ({ children }: Props) => {
 
   const domain = import.meta.env.VITE_AUTH0_DOMAIN;
   const clientId = import.meta.env.VITE_AUTH0_CLIENT_ID;
-  const redirectUri =
-    import.meta.env.MODE === "development"
-      ? import.meta.env.VITE_AUTH0_CALLBACK_URL_DEV
-      : import.meta.env.VITE_AUTH0_CALLBACK_URL_PRO;
+  const redirectUri = import.meta.env.VITE_AUTH0_CALLBACK_URL;
   const audience = import.meta.env.VITE_AUTH0_AUDIENCE;
 
   if (!domain || !clientId || !redirectUri || !audience) {
